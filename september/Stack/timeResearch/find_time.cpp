2dedad4be9c2d3434dd6cd78957b668d0f1ab997
includealex <mishin.aa@phystech.edu>
2021-10-12 18:32:14 +0300

    proved that best factor is smth between 1.7 and 1.8

---
+++ september/Stack/timeResearch/find_time.cpp
@@ -0,0 +1,32 @@
+#include <benchmark/benchmark.h>
+
+static void stackBench(benchmark::State &addit) {
+    int len = addit.range(0);
+    double factor = 1 + len / 10;
+
+    int counter_ = 1;
+    int size_ = 1;
+
+
+    for(auto _ : addit) {
+        int* data_ = new int[32];
+        for (int i = 0; i < 1000; ++i) {
+            if(counter_ = size_) {
+                    int* tmp = data_;
+                    data_ = new int[int(size_ * factor)];
+                    std::copy(tmp, tmp + size_, data_);
+                    size_ = (int)size_ * factor;
+            }
+
+            data_[counter_++] = i;
+        }
+        delete[] data_;
+    }
+
+}
+
+
+
+BENCHMARK(stackBench)->DenseRange(4, 9)->Unit(benchmark::kMillisecond);
+
+BENCHMARK_MAIN();

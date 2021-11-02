cbfd0886259a841dadbb98d2fc3fd521c5607637
includealex <mishin.aa@phystech.edu>
2021-11-02 19:36:35 +0300

    added header

--- september/Stack/includes/Stack.h
+++ september/Stack/includes/Stack.h
@@ -4,35 +4,35 @@
 #include "Stack_impl.h"
 
 template <>
-class Stack <bool> {
-public:
-    size_t START_STACK_SIZE = 32;
+class Stack<bool> {
+ public:
+  size_t START_STACK_SIZE = 32;
 
-    Stack();
-    Stack(const Stack& other);
-    Stack(Stack&& other) noexcept;
-    ~Stack();
+  Stack();
+  Stack(const Stack& other);
+  Stack(Stack&& other) noexcept;
+  ~Stack();
 
-    size_t size() const;
+  size_t size() const;
 
-    bool top() const;
+  bool top() const;
 
-    void pop();
-    void push(bool rhs);
-    void swap(Stack<bool>& other);
+  void pop();
+  void push(bool rhs);
+  void swap(Stack<bool>& other);
 
-    bool is_empty() const;
-    bool operator== (const Stack<bool>& other) const;
-    bool operator!= (const Stack<bool>& other) const;
+  bool is_empty() const;
+  bool operator==(const Stack<bool>& other) const;
+  bool operator!=(const Stack<bool>& other) const;
 
-    Stack& operator= (const Stack& other);
-    Stack& operator= (Stack&& other) noexcept;
+  Stack& operator=(const Stack& other);
+  Stack& operator=(Stack&& other) noexcept;
 
-private:
-    uint32_t* data_;
-    size_t size_;
-    size_t counter_;
-    void stack_realloc();
+ private:
+  uint32_t* data_;
+  size_t size_;
+  size_t counter_;
+  void stack_realloc();
 };
 
-#endif //STACK_INCLUDES_STACK_H_
+#endif  // STACK_INCLUDES_STACK_H_

a18501fae7f18b50476c594453e7e14a89425d18
includealex <mishin.aa@phystech.edu>
2021-11-08 03:43:35 +0300

    changed some functions

--- september/Stack/Stack/source/boolStack.cpp
+++ september/Stack/Stack/source/boolStack.cpp
@@ -29,21 +29,14 @@ size_t Stack<bool>::size() const {
 }
 
 bool Stack<bool>::is_empty() const {
-  return counter_ == 0 /*? true : false*/;
+  return counter_ == 0;
 }
 
-// bool Stack<bool>::top() const {
-//    uint32_t containElem = data_[size_ - (counter_ / 32)];
-//    containElem  = containElem >> ((counter_ - 1) % 32);
-//    containElem &= 1;
-//    return containElem;
-//}
-
 bool Stack<bool>::top() const {
-  uint32_t num_of_bit = counter_ % 32 - 1;
-  uint32_t num_of_byte = counter_ / 32;
+  auto num_of_bit = counter_ % 32 - 1;
+  auto num_of_byte = counter_ / 32;
 
-  bool elem = static_cast<bool>((data_[num_of_byte] & (1 << num_of_bit)) >> num_of_bit);
+  bool elem = static_cast<bool>((data_[num_of_byte] & ((static_cast<unsigned int>(1)) << num_of_bit)) >> num_of_bit);
 
   return elem;
 }
@@ -57,36 +50,18 @@ void Stack<bool>::pop() {
   counter_--;
 }
 
-// void Stack<bool>::push(bool rhs) {
-//    if (counter_ == size_) {
-//        stack_realloc();
-//    }
-//
-//    uint32_t num_of_bit  = 32 - 1 - (uint32_t)(counter_ % 8);
-//    uint32_t num_of_byte = (uint32_t)(size_ - 1 - counter_ / 8);
-//
-//    if(rhs == 1) {
-//        data_[num_of_byte] = (1 << num_of_bit);
-//    }
-//    else if(rhs == 0) {
-//        data_[num_of_byte] &= ~(1 << num_of_bit);
-//    }
-//
-//    counter_++;
-//}
-
 void Stack<bool>::push(bool rhs) {
   if (counter_ == size_) {
     stack_realloc();
   }
 
   uint32_t num_of_bit = counter_ % 32;
-  uint32_t num_of_byte = counter_ / 32;
+  auto num_of_byte = static_cast<uint32_t>(counter_ / 32);
 
   if (rhs) {
-    data_[num_of_byte] = (1 << num_of_bit);
+    data_[num_of_byte] = ((static_cast<unsigned int>(1)) << num_of_bit);
   } else if (!rhs) {
-    data_[num_of_byte] &= ~(1 << num_of_bit);
+    data_[num_of_byte] &= ~((static_cast<unsigned int>(1)) << num_of_bit);
   }
 
   counter_++;
@@ -98,43 +73,13 @@ void Stack<bool>::swap(Stack<bool>& other) {
   other = std::move(tmp);
 }
 
-// bool Stack<bool>::operator== (const Stack<bool>& other) const{
-//    if (counter_ != other.counter_) {
-//        return false;
-//    }
-//
-//    for (uint32_t i = 0; i < size_ / 2; ++i) {
-//        if (data_[i] != other.data_[i])
-//            return false;
-//    }
-//
-//    size_t oth_count = (counter_ % (size_ * 16)) / 32;
-//
-//    for(uint32_t i = 0; i < oth_count; ++i) {
-//        if(data_[size_ - i] != other.data_[size_ - i]) {
-//            return false;
-//        }
-//    }
-//
-//    uint32_t lhs = data_[size_ - oth_count];
-//    uint32_t rhs = other.data_[size_ - oth_count];
-//
-//    size_t bit_val = (counter_ % (size_ * 16)) % 32;
-//
-//    if((lhs % bit_val) != (rhs % bit_val)) {
-//        return false;
-//    }
-//
-//    return true;
-//}
-
 bool Stack<bool>::operator==(const Stack<bool>& other) const {
   if (counter_ != other.counter_) {
     return false;
   }
 
   uint32_t num_of_bit = counter_ % 32;
-  uint32_t num_of_byte = counter_ / 32;
+  auto num_of_byte = static_cast<uint32_t>(counter_ / 32);
 
   for (uint32_t i = 0; i < num_of_byte; ++i) {
     if (data_[i] != other.data_[i]) {
@@ -143,7 +88,7 @@ bool Stack<bool>::operator==(const Stack<bool>& other) const {
   }
 
   for (uint32_t i = 0; i < num_of_bit; ++i) {
-    if ((data_[num_of_byte] & (1 << i)) != (other.data_[num_of_byte] & (1 << i))) {
+    if ((data_[num_of_byte] & ((static_cast<unsigned int>(1)) << i)) != (other.data_[num_of_byte] & ((static_cast<unsigned int>(1)) << i))) {
       return false;
     }
   }
@@ -183,7 +128,7 @@ Stack<bool>& Stack<bool>::operator=(Stack<bool>&& other) noexcept {
 
 void Stack<bool>::stack_realloc() {
   uint32_t* tmp = data_;
-  //    delete[] data_;
+  delete[] data_;
   auto new_size = static_cast<uint32_t>(size_ * 2);
   data_ = new uint32_t[new_size];
   std::copy(tmp, tmp + size_, data_);

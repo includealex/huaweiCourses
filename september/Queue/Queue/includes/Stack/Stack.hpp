6f23e4cbff4300dceaeecd2988b1b56d57ab6c8c
includealex <mishin.aa@phystech.edu>
2021-12-04 14:36:31 +0300

    working Stack

---
+++ september/Queue/Queue/includes/Stack/Stack.hpp
@@ -0,0 +1,38 @@
+#ifndef STACK_INCLUDES_STACK_H_
+#define STACK_INCLUDES_STACK_H_
+
+#include "Stack_impl.hpp"
+
+template <>
+class Stack<bool> {
+ public:
+  size_t START_STACK_SIZE = 32;
+
+  Stack();
+  Stack(const Stack& other);
+  Stack(Stack&& other) noexcept;
+  ~Stack();
+
+  size_t size() const;
+
+  bool top() const;
+
+  void pop();
+  void push(bool rhs);
+  void swap(Stack<bool>& other);
+
+  bool is_empty() const;
+  bool operator==(const Stack<bool>& other) const;
+  bool operator!=(const Stack<bool>& other) const;
+
+  Stack& operator=(const Stack& other);
+  Stack& operator=(Stack&& other) noexcept;
+
+ private:
+  uint32_t* data_;
+  size_t size_;
+  size_t counter_;
+  void stack_realloc();
+};
+
+#endif  // STACK_INCLUDES_STACK_H_
